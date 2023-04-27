8aaf8578c87f4ecdb901ffe93d76dd30004006fc
ismael <ismael.bortoluzzi77@gmail.com>
2023-04-26 21:55:05 -0300

    subindo o inicio da ordem de servico

---
+++ ordem_servico/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from ordem_servico.models import OrdemServico
+
+
+class OrdemServicoForm(forms.ModelForm):
+    PROCESSO_CHOICES = (
+        ('ORC', 'Orçamento'),
+        ('DEV', 'Desenvolvimento'),
+        ('PAG', 'Pagamento'),
+    )
+    processo = forms.ChoiceField(choices=PROCESSO_CHOICES)
+
+    STATUS_CHOICES = (
+        ('ABE', 'Aberta'),
+        ('ASS', 'Assumida'),
+        ('FIN', 'Finalizada'),
+    )
+    status = forms.ChoiceField(choices=STATUS_CHOICES)
+
+    def clean(self):
+        demanda = self.cleaned_data.get('demanda')
+        processo = self.cleaned_data.get('processo')
+        return super().clean()
+
+    class Meta:
+        model = OrdemServico
+        fields = ('demanda', 'processo',)
--- ordem_servico/views.py
+++ ordem_servico/views.py
@@ -1,3 +1,26 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from ordem_servico.forms import OrdemServicoForm
+
+
+def create_ordem_servico(request):
+    if request.method == 'GET':
+        context = {
+            'ordem_servico_form': OrdemServicoForm(),
+        }
+        return render(request, 'ordem_servico/create_ordem_servico.html', context)
+
+    if request.method == 'POST':
+        order_form = OrdemServicoForm(request.POST)
+
+        if order_form.is_valid():
+            new_order = order_form.save()
+
+            messages.success(request, 'Salvo Com Sucesso!')
+        else:
+            messages.error(request, 'Não foi possível salvar a ordem de serviço!')
+
+        return HttpResponseRedirect(reverse('list-order'))
