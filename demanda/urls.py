11862eb098da7aea8af9ff90f0a6f0ed3464261e
Nícolas R. Hackenhar <prog46@ixcsoft.com.br>
2023-05-10 21:32:30 -0300

    Adicionado remoção de demanda.

--- demanda/urls.py
+++ demanda/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('create/', create_demanda, name='create-demanda'),
     path('list/', ListDemanda.as_view(), name='list-demanda'),
-     path('edit/<int:pk>', edit_demanda, name='edit-demanda'),
+    path('edit/<int:pk>', edit_demanda, name='edit-demanda'),
+    path('delete/<int:pk>', delete_demanda, name='delete-demanda'),
 ]
