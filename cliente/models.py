16e7ead854faca33f402ca4c0258d5ba9d2a4260
Nícolas R. Hackenhar <prog46@ixcsoft.com.br>
2023-05-17 20:46:50 -0300

    Adicionada máscara de CPF e CNPJ para o cliente.

--- cliente/models.py
+++ cliente/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=16, unique=False)
-    documento = models.CharField(max_length=14, unique=True)
+    documento = models.CharField(max_length=18, unique=True)
     telefone = models.CharField(max_length=16, null=True, blank=True, default='')
     email = models.EmailField(max_length=255, default='')
 
